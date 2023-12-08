e3bc7f181ee3a0e5ba567c7fc750050282719f5b
David Schmid <david.schmid@mailbox.org>
2023-12-08 17:28:29 +0100

    Refactor on-exit.tgs.sh script
    - Change shebang from `#!/bin/sh` to `#!/bin/bash`
    - Fix read command to use `-r` flag for improved behavior
    - Add error handling for changing directory to `~/.task`
    - Add check for `.data` files in git status
    - Add commit message with timestamp for task updates
    - Add error handling for commit, pull, and push commands
    - Print message "No changes to commit." if no `.data` files found
    - Print operation completion message with parameter

--- on-exit.tgs.sh
+++ on-exit.tgs.sh
@@ -1,18 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
 DIRTY=0
 
-while read x
+while read -r
 do
     DIRTY=1
 done
 
 if [ ${DIRTY} -eq 1 ]
 then
-    cd ~/.task
-    git commit -m "Hook Commit" *.data
-    git pull -q
-    git push -q
+    cd ~/.task || { echo "Error: Failed to change directory to ~/.task"; exit 1; }
+
+    if git status | grep -q "\.data"; then
+        TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+        git commit -m "Task Update at ${TIMESTAMP}" ./*.data || { echo "Error: Commit failed"; exit 1; }
+        git pull -q || { echo "Error: Pull failed"; exit 1; }
+        git push -q || { echo "Error: Push failed"; exit 1; }
+    else
+        echo "No changes to commit."
+    fi
 fi
 
-echo ${1}
+echo "Operation completed. Parameter: ${1}"
